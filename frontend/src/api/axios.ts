7e35adda49470ce8a394a174ef680616b58bf195
Tautik Agrahari <tautikagrahari@gmail.com>
2025-02-17 12:06:44 +0530

    test-hardcoded url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { showToast } from '../utils/toast';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1';
+const baseURL = 'https://leads-artisan-backend.fly.dev/api/v1'
 
 export const api = axios.create({
   baseURL,
